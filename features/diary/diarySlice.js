c1624926b70d5f7f2d136203d4bb500449599b46
Krista Frietas <krista.frietas@gmail.com>
2023-05-17 10:57:01 -0400

    Redux

---
+++ features/diary/diarySlice.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { baseUrl } from '../../shared/baseUrl';
+
+export const fetchDiary = createAsyncThunk(
+    'comments/fetchDiary',
+    async () => {
+        const response = await fetch(baseUrl + 'comments');
+        if (!response.ok) {
+            return Promise.reject(
+                'Unable to fetch, status: ' + response.status
+            );
+        }
+        const data = await response.json();
+        return data;
+    }
+);
+
+const diarySlice = createSlice({
+    name: 'diary',
+    initialState: { isLoading: true, errMess: null, diaryArray: [] },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchDiary.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(fetchDiary.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.errMess = null;
+                state.diaryArray = action.payload;
+            })
+            .addCase(fetchDiary.rejected, (state, action) => {
+                state.isLoading = false;
+                state.errMess = action.error
+                    ? action.error.message
+                    : 'Fetch failed';
+            });
+    }
+});
+
+export const diaryReducer = diarySlice.reducer;
---
+++ redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { diaryReducer } from '../features/diary/diarySlice';
+
+
+export const store = configureStore({
+    reducer: {
+        diary: diaryReducer
+    }
+});
